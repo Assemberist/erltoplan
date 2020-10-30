7e96ba8d37bd75bd2b834d2ccce48e22083ba1c0
Assemberist <schur.alexandr2015@yandex.ru>
2020-10-30 12:18:28 +0300

    participly filled termanus.hrl

--- etoplan.erl
+++ etoplan.erl
@@ -1,11 +1,14 @@
 -module(etoplan).
 
+-include("termanus.hrl").
+
 -export([parse/1]).
 -export([parself/0]).
 
 parself() -> parse(?FILE).
 
 parse(File) ->
+	erlout:init(File),
 	{ok, Src} = epp:parse_file(File, []),
 	
 	Extports = 
@@ -13,7 +16,7 @@ parse(File) ->
 			lists:foldl(
 				fun(A, Acc)->
 					case A of
-						{attribute, _, export, FunList} -> FunList ++ Acc;
+						?attr_export -> A#attribute.value ++ Acc;
 						_ -> Acc
 					end
 				end, 
@@ -21,6 +24,26 @@ parse(File) ->
 			)
 		),
 		
-	{ok, Src}.
-		
+	erlout:write_export(File, Extports),
 	
+	Functionss = 
+		proplists:get_keys(
+			lists:foldl(
+				fun(A, Acc)->
+					case A of
+						#function{} -> A ++ Acc;
+						_ -> Acc
+					end
+				end, 
+				[], Src
+			)
+		),
+
+	[get_calls(Fun#function.name, Fun#function.enrtyes) || Fun <= Functions],
+	erlout:finite(File).
+		
+get_calls(Fun, Entry) ->
+	case Entry of 
+		?attr_export -> erlout:write_link(File, {Fun, Entry#attribute.value});
+		_ -> ok;
+	end.
